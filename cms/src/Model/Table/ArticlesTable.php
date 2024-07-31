bea303da3eaaefe9e276e72bd13b5980f8c63300
Swalden12 <Samuel.walden@hydracor.net>
2024-07-31 18:25:02 +0000

    Creating the Articles Controller

--- cms/src/Model/Table/ArticlesTable.php
+++ cms/src/Model/Table/ArticlesTable.php
@@ -1,8 +1,15 @@
 <?php
 // src/Model/Table/ArticlesTable.php
 namespace App\Model\Table;
+// add this use statement right below the namespace declaration to import
+// the Validator class
+use Cake\Validation\Validator;
+
 
 use Cake\ORM\Table;
+use Cake\Utility\Text;
+// the EventInterface class
+use Cake\Event\EventInterface;
 
 class ArticlesTable extends Table
 {
@@ -10,4 +17,24 @@ class ArticlesTable extends Table
     {
         $this->addBehavior('Timestamp');
     }
+    public function beforeSave(EventInterface $event, $entity, $options)
+{
+    if ($entity->isNew() && !$entity->slug) {
+        $sluggedTitle = Text::slug($entity->title);
+        // trim slug to maximum length defined in schema
+        $entity->slug = substr($sluggedTitle, 0, 191);
+    }
+}
+public function validationDefault(Validator $validator): Validator
+{
+    $validator
+        ->notEmptyString('title')
+        ->minLength('title', 10)
+        ->maxLength('title', 255)
+
+        ->notEmptyString('body')
+        ->minLength('body', 10);
+
+    return $validator;
+}
 }
