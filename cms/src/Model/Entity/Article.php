48527aac56cf63dc861b85d6d15408c32dcc68a1
Swalden12 <Samuel.walden@hydracor.net>
2024-08-01 16:54:55 +0000

    Site Test1

--- cms/src/Model/Entity/Article.php
+++ cms/src/Model/Entity/Article.php
@@ -1,31 +1,71 @@
 <?php
-// src/Model/Entity/Article.php
+declare(strict_types=1);
+
 namespace App\Model\Entity;
-// the Collection class
+
 use Cake\Collection\Collection;
-
 use Cake\ORM\Entity;
+use Cake\Utility\Text;
 
+/**
+ * Article Entity
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $title
+ * @property string $slug
+ * @property string|null $body
+ * @property bool|null $published
+ * @property \Cake\I18n\DateTime|null $created
+ * @property \Cake\I18n\DateTime|null $modified
+ *
+ * @property \App\Model\Entity\User $user
+ * @property \App\Model\Entity\Tag[] $tags
+ */
 class Article extends Entity
 {
-// Update the accessible property to contain `tag_string`
-protected $_accessible = [
-    //other fields...
-    'tag_string' => true
-];
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected array $_accessible = [
+        'user_id' => true,
+        'title' => true,
+        'slug' => true,
+        'body' => true,
+        'published' => true,
+        'created' => true,
+        'modified' => true,
+        'user' => true,
+        'tags' => true,
+        'tag_string' => true,
+    ];
 
-protected function _getTagString()
-{
-    if (isset($this->_fields['tag_string'])) {
-        return $this->_fields['tag_string'];
+    protected function _setTitle(string $title): string
+    {
+        $this->slug = Text::slug($title);
+
+        return $title;
     }
-    if (empty($this->tags)) {
-        return '';
+
+    protected function _getTagString(): string
+    {
+        if (isset($this->_fields['tag_string'])) {
+            return $this->_fields['tag_string'];
+        }
+        if (empty($this->tags)) {
+            return '';
+        }
+        $tags = new Collection($this->tags);
+        $str = $tags->reduce(function ($string, $tag) {
+            return $string . $tag->title . ', ';
+        }, '');
+
+        return trim($str, ', ');
     }
-    $tags = new Collection($this->tags);
-    $str = $tags->reduce(function ($string, $tag) {
-        return $string . $tag->title . ', ';
-    }, '');
-    return trim($str, ', ');
-}
 }
