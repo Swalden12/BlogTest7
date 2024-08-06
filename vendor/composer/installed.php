01fc332e39f2ed8392b17647f1123fd55554a724
Swalden12 <Samuel.walden@hydracor.net>
2024-08-06 17:45:33 +0000

    Authorization Plugin reinstall

---
+++ vendor/composer/installed.php
@@ -0,0 +1,303 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '48527aac56cf63dc861b85d6d15408c32dcc68a1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '48527aac56cf63dc861b85d6d15408c32dcc68a1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'cakephp/authorization' => array(
+            'pretty_version' => '2.4.0',
+            'version' => '2.4.0.0',
+            'reference' => '94fadea13efdc624e1da7a5c1c35f962ba78cc98',
+            'type' => 'cakephp-plugin',
+            'install_path' => __DIR__ . '/../cakephp/authorization',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'cakephp/cache' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.5.6',
+            ),
+        ),
+        'cakephp/cakephp' => array(
+            'pretty_version' => '4.5.6',
+            'version' => '4.5.6.0',
+            'reference' => '39aa903620065161dc0d2bdcdaa440e887f574d0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../cakephp/cakephp',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'cakephp/chronos' => array(
+            'pretty_version' => '2.4.5',
+            'version' => '2.4.5.0',
+            'reference' => 'b0321ab7658af9e7abcb3dd876f226e6f3dbb81f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../cakephp/chronos',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'cakephp/collection' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.5.6',
+            ),
+        ),
+        'cakephp/console' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.5.6',
+            ),
+        ),
+        'cakephp/core' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.5.6',
+            ),
+        ),
+        'cakephp/database' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.5.6',
+            ),
+        ),
+        'cakephp/datasource' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.5.6',
+            ),
+        ),
+        'cakephp/event' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.5.6',
+            ),
+        ),
+        'cakephp/filesystem' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.5.6',
+            ),
+        ),
+        'cakephp/form' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.5.6',
+            ),
+        ),
+        'cakephp/http' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.5.6',
+            ),
+        ),
+        'cakephp/i18n' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.5.6',
+            ),
+        ),
+        'cakephp/log' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.5.6',
+            ),
+        ),
+        'cakephp/orm' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.5.6',
+            ),
+        ),
+        'cakephp/utility' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.5.6',
+            ),
+        ),
+        'cakephp/validation' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.5.6',
+            ),
+        ),
+        'composer/ca-bundle' => array(
+            'pretty_version' => '1.5.1',
+            'version' => '1.5.1.0',
+            'reference' => '063d9aa8696582f5a41dffbbaf3c81024f0a604a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/./ca-bundle',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'laminas/laminas-diactoros' => array(
+            'pretty_version' => '2.26.0',
+            'version' => '2.26.0.0',
+            'reference' => '6584d44eb8e477e89d453313b858daac6183cddc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laminas/laminas-diactoros',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'laminas/laminas-httphandlerrunner' => array(
+            'pretty_version' => '2.10.0',
+            'version' => '2.10.0.0',
+            'reference' => '35a0ba92e940a2f9533754f5a56187fa321f7693',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laminas/laminas-httphandlerrunner',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'league/container' => array(
+            'pretty_version' => '4.2.2',
+            'version' => '4.2.2.0',
+            'reference' => 'ff346319ca1ff0e78277dc2311a42107cc1aab88',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../league/container',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'orno/di' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '~2.0',
+            ),
+        ),
+        'psr/container' => array(
+            'pretty_version' => '2.0.2',
+            'version' => '2.0.2.0',
+            'reference' => 'c71ecc56dfe541dbd90c5360474fbc405f8d5963',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/container',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/container-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '^1.0',
+                1 => '^1.0 || ^2.0',
+            ),
+        ),
+        'psr/http-client' => array(
+            'pretty_version' => '1.0.3',
+            'version' => '1.0.3.0',
+            'reference' => 'bb5906edc1c324c9a05aa0873d40117941e5fa90',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/http-client',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/http-client-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '^1.0',
+            ),
+        ),
+        'psr/http-factory' => array(
+            'pretty_version' => '1.1.0',
+            'version' => '1.1.0.0',
+            'reference' => '2b4765fddfe3b508ac62f829e852b1501d3f6e8a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/http-factory',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/http-factory-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0',
+            ),
+        ),
+        'psr/http-message' => array(
+            'pretty_version' => '1.1',
+            'version' => '1.1.0.0',
+            'reference' => 'cb6ce4845ce34a8ad9e68117c10ee90a29919eba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/http-message',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/http-message-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0',
+            ),
+        ),
+        'psr/http-server-handler' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => '84c4fb66179be4caaf8e97bd239203245302e7d4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/http-server-handler',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/http-server-handler-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '^1.0',
+            ),
+        ),
+        'psr/http-server-middleware' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => 'c1481f747daaa6a0782775cd6a8c26a1bf4a3829',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/http-server-middleware',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/http-server-middleware-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '^1.0',
+            ),
+        ),
+        'psr/log' => array(
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
+            'reference' => 'ef29f6d262798707a9edd554e2b82517ef3a9376',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/log-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '^1.0 || ^2.0',
+            ),
+        ),
+        'psr/simple-cache' => array(
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
+            'reference' => '8707bf3cea6f710bf6ef05491234e3ab06f6432a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/simple-cache',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/simple-cache-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '^1.0 || ^2.0',
+            ),
+        ),
+    ),
+);
