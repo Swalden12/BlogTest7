6ae691edcacfc11ad29d18a33479a4b59087620c
Swalden12 <Samuel.walden@hydracor.net>
2024-08-20 18:21:26 +0000

    Application.php file fix

--- cms/src/Application.php
+++ cms/src/Application.php
@@ -1,4 +1,19 @@
 <?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     3.3.0
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ */
 namespace App;
 
 use Cake\Core\Configure;
@@ -27,48 +42,77 @@ use Authorization\Middleware\AuthorizationMiddleware;
 use Authorization\Policy\OrmResolver;
 
 class Application extends BaseApplication
-    implements AuthenticationServiceProviderInterface,
-    AuthorizationServiceProviderInterface
+    implements AuthenticationServiceProviderInterface, AuthorizationServiceProviderInterface
+
 {
+    /**
+     * Load all the application configuration and bootstrap logic.
+     *
+     * @return void
+     */
     public function bootstrap(): void
     {
+        // Call parent to load bootstrap from files.
         parent::bootstrap();
 
         if (PHP_SAPI === 'cli') {
             $this->bootstrapCli();
+        } else {
+            FactoryLocator::add(
+                'Table',
+                (new TableLocator())->allowFallbackClass(false)
+            );
         }
 
+        /*
+         * Only try to load DebugKit in development mode
+         * Debug Kit should not be installed on a production system
+         */
         if (Configure::read('debug')) {
             $this->addPlugin('DebugKit');
         }
-        $this->addPlugin('Authentication');
         $this->addPlugin('Authorization');
+        // Load more plugins here
     }
 
-    protected function bootstrapCli(): void
-    {
-        try {
-            $this->addPlugin('Bake');
-        } catch (MissingPluginException $e) {
-            // Do not halt if the plugin is missing
-        }
-
-        $this->addPlugin('Migrations');
-    }
-
-    public function middleware($middlewareQueue): \Cake\Http\MiddlewareQueue
+    /**
+     * Setup the middleware queue your application will use.
+     *
+     * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup.
+     * @return \Cake\Http\MiddlewareQueue The updated middleware queue.
+     */
+    public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue
     {
         $middlewareQueue
-            ->add(ErrorHandlerMiddleware::class)
-            ->add(AssetMiddleware::class)
-            ->add(new BodyParserMiddleware())
-            ->add(new RoutingMiddleware($this))
-            ->add(new AuthenticationMiddleware($this))
-            ->add(new AuthorizationMiddleware($this));
+            // Catch any exceptions in the lower layers,
+            // and make an error page/response
+            ->add(new ErrorHandlerMiddleware(Configure::read('Error'), $this))
 
-        if (Configure::read('debug')) {
-            Configure::write('DebugKit.ignoreAuthorization', true);
-        }
+            // Handle plugin/theme assets like CakePHP normally does.
+            ->add(new AssetMiddleware([
+                'cacheTime' => Configure::read('Asset.cacheTime'),
+            ]))
+
+            // Add routing middleware.
+            // If you have a large number of routes connected, turning on routes
+            // caching in production could improve performance.
+            // See https://github.com/CakeDC/cakephp-cached-routing
+            ->add(new RoutingMiddleware($this))
+
+            // Parse various types of encoded request bodies so that they are
+            // available as array through $request->getData()
+            // https://book.cakephp.org/4/en/controllers/middleware.html#body-parser-middleware
+            ->add(new BodyParserMiddleware())
+
+            // AuthenticationMiddleware should be after routing and body parser
+            ->add(new AuthenticationMiddleware($this))
+            // Cross Site Request Forgery (CSRF) Protection Middleware
+            // https://book.cakephp.org/4/en/security/csrf.html#cross-site-request-forgery-csrf-middleware
+            ->add(new CsrfProtectionMiddleware([
+                'httponly' => true,    
+            ]))
+
+            ->add(new AuthorizationMiddleware($this));
 
         return $middlewareQueue;
     }
@@ -80,16 +124,17 @@ class Application extends BaseApplication
             'queryParam' => 'redirect',
         ]);
 
-        // Load identifiers, ensure we check email and password fields
+        // Load identifiers, ensuring we check email and password fields
         $authenticationService->loadIdentifier('Authentication.Password', [
             'fields' => [
                 'username' => 'email',
                 'password' => 'password',
             ],
         ]);
-
-        // Load the authenticators, you want session first
+        
+        // Loading the authenticators, we want session first
         $authenticationService->loadAuthenticator('Authentication.Session');
+        // Configure form data check to pick email and password
         $authenticationService->loadAuthenticator('Authentication.Form', [
             'fields' => [
                 'username' => 'email',
@@ -104,6 +149,34 @@ class Application extends BaseApplication
     public function getAuthorizationService(ServerRequestInterface $request): AuthorizationServiceInterface
     {
         $resolver = new OrmResolver();
+
         return new AuthorizationService($resolver);
+    }
+
+    /**
+     * Register application container services.
+     *
+     * @param \Cake\Core\ContainerInterface $container The Container to update.
+     * @return void
+     * @link https://book.cakephp.org/4/en/development/dependency-injection.html#dependency-injection
+     */
+    public function services(ContainerInterface $container): void
+    {
+    }
+
+    /**
+     * Bootstrapping for CLI application.
+     *
+     * That is when running commands.
+     *
+     * @return void
+     */
+    protected function bootstrapCli(): void
+    {
+        $this->addOptionalPlugin('Bake');
+
+        $this->addPlugin('Migrations');
+
+        // Load more plugins here
     }
 }
--- cms/src/Policy/ArticlePolicy.php
+++ cms/src/Policy/ArticlePolicy.php
@@ -1,30 +1,62 @@
 <?php
+declare(strict_types=1);
+
 namespace App\Policy;
 
 use App\Model\Entity\Article;
 use Authorization\IdentityInterface;
-
+use App\Policy\ServerRequest;
+/**
+ * Article policy
+ */
 class ArticlePolicy
 {
-    public function canAdd(IdentityInterface $user, Article $article)
+    /**
+     * Check if $user can add Article
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Article $article
+     * @return bool
+     */
+    
+     public function canAdd(IdentityInterface $user, Article $article)
     {
-        // All logged in users can create articles.
         return true;
     }
 
+    /**
+     * Check if $user can edit Article
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Article $article
+     * @return bool
+     */
     public function canEdit(IdentityInterface $user, Article $article)
     {
-        // logged in users can edit their own articles.
         return $this->isAuthor($user, $article);
     }
 
+    /**
+     * Check if $user can delete Article
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Article $article
+     * @return bool
+     */
     public function canDelete(IdentityInterface $user, Article $article)
     {
-        // logged in users can delete their own articles.
         return $this->isAuthor($user, $article);
     }
 
-    protected function isAuthor(IdentityInterface $user, Article $article)
+    /**
+     * Check if $user can view Article
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Article $article
+     * @return bool
+     */
+    
+     public function isAuthor(IdentityInterface $user, Article $article)
     {
         return $article->user_id === $user->getIdentifier();
     }
