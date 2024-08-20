55de461c81493fa67b699cf62442759a16e28f9b
Swalden12 <Samuel.walden@hydracor.net>
2024-08-20 13:57:31 +0000

    Middleware Update

--- cms/src/Application.php
+++ cms/src/Application.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link      https://cakephp.org CakePHP(tm) Project
- * @since     3.3.0
- * @license   https://opensource.org/licenses/mit-license.php MIT License
- */
 namespace App;
 
 use Authentication\AuthenticationService;
@@ -30,36 +17,20 @@ use Cake\Http\BaseApplication;
 use Cake\Routing\Middleware\AssetMiddleware;
 use Cake\Routing\Middleware\RoutingMiddleware;
 use Cake\Routing\Router;
-use Cake\Http\Middleware\BodyParserMiddleware; // Import the BodyParserMiddleware
+use Cake\Http\Middleware\BodyParserMiddleware;
 
-/**
- * Application setup class.
- *
- * This defines the bootstrapping logic and middleware layers you
- * want to use in your application.
- */
 class Application extends BaseApplication
     implements AuthenticationServiceProviderInterface,
     AuthorizationServiceProviderInterface
 {
-    /**
-     * Load all the application configuration and bootstrap logic.
-     *
-     * @return void
-     */
     public function bootstrap(): void
     {
-        // Call parent to load bootstrap from files.
         parent::bootstrap();
 
         if (PHP_SAPI === 'cli') {
             $this->bootstrapCli();
         }
 
-        /*
-         * Only try to load DebugKit in development mode
-         * Debug Kit should not be installed on a production system
-         */
         if (Configure::read('debug')) {
             $this->addPlugin('DebugKit');
         }
@@ -67,13 +38,6 @@ class Application extends BaseApplication
         $this->addPlugin('Authorization');
     }
 
-    /**
-     * Bootstrapping for CLI application.
-     *
-     * That is when running commands.
-     *
-     * @return void
-     */
     protected function bootstrapCli(): void
     {
         try {
@@ -83,36 +47,16 @@ class Application extends BaseApplication
         }
 
         $this->addPlugin('Migrations');
-
-        // Load more plugins here
     }
 
-    /**
-     * Setup the middleware queue your application will use.
-     *
-     * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup.
-     * @return \Cake\Http\MiddlewareQueue The updated middleware queue.
-     */
     public function middleware($middlewareQueue): \Cake\Http\MiddlewareQueue
     {
         $middlewareQueue
-            // Catch any exceptions in the lower layers,
-            // and make an error page/response
             ->add(ErrorHandlerMiddleware::class)
-
-            // Handle plugin/theme assets like CakePHP normally does.
             ->add(AssetMiddleware::class)
-
-            // Parse body data
-            ->add(new BodyParserMiddleware()) // Add BodyParserMiddleware here
-
-            // Add routing middleware.
+            ->add(new BodyParserMiddleware())
             ->add(new RoutingMiddleware($this))
-
-            // Add Authentication after RoutingMiddleware
             ->add(new AuthenticationMiddleware($this))
-
-            // Add Authorization after Authentication
             ->add(new AuthorizationMiddleware($this));
 
         if (Configure::read('debug')) {
@@ -139,7 +83,6 @@ class Application extends BaseApplication
 
         // Load the authenticators, you want session first
         $authenticationService->loadAuthenticator('Authentication.Session');
-        // Configure form data check to pick email and password
         $authenticationService->loadAuthenticator('Authentication.Form', [
             'fields' => [
                 'username' => 'email',
@@ -154,7 +97,7 @@ class Application extends BaseApplication
     public function getAuthorizationService(ServerRequestInterface $request): AuthorizationServiceInterface
     {
         $resolver = new OrmResolver();
-
         return new AuthorizationService($resolver);
     }
 }
+3
