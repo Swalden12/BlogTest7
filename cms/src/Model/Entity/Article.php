f4f1662be4fcc0f9c29b62c80809ca0b67c14720
Swalden12 <Samuel.walden@hydracor.net>
2024-08-21 13:19:41 +0000

    Article.php formatting

--- cms/src/Model/Entity/Article.php
+++ cms/src/Model/Entity/Article.php
@@ -1,59 +1,26 @@
 <?php
+// src/Model/Entity/Article.php
 declare(strict_types=1);
 
 namespace App\Model\Entity;
-
+//Import the Collection class
 use Cake\Collection\Collection;
 use Cake\ORM\Entity;
-use Cake\Utility\Text;
 
-/**
- * Article Entity
- *
- * @property int $id
- * @property int $user_id
- * @property string $title
- * @property string $slug
- * @property string|null $body
- * @property bool|null $published
- * @property \Cake\I18n\DateTime|null $created
- * @property \Cake\I18n\DateTime|null $modified
- *
- * @property \App\Model\Entity\User $user
- * @property \App\Model\Entity\Tag[] $tags
- */
+
 class Article extends Entity
 {
-    /**
-     * Fields that can be mass assigned using newEntity() or patchEntity().
-     *
-     * Note that when '*' is set to true, this allows all unspecified fields to
-     * be mass assigned. For security purposes, it is advised to set '*' to false
-     * (or remove it), and explicitly make individual fields accessible as needed.
-     *
-     * @var array
-     */
     protected array $_accessible = [
-        'user_id' => true,
         'title' => true,
-        'slug' => true,
         'body' => true,
         'published' => true,
         'created' => true,
         'modified' => true,
-        'user' => true,
-        'tags' => true,
-        'tag_string' => true,
+        'users' => true,
+        'tag_string' => true
     ];
 
-    protected function _setTitle(string $title): string
-    {
-        $this->slug = Text::slug($title);
-
-        return $title;
-    }
-
-    protected function _getTagString(): string
+    protected function _getTagString()
     {
         if (isset($this->_fields['tag_string'])) {
             return $this->_fields['tag_string'];
