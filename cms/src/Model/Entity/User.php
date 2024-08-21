fcb78b24b57d6d374271386b9b73ae2cbb009dcb
Swalden12 <Samuel.walden@hydracor.net>
2024-08-21 13:08:08 +0000

    user.php update

--- cms/src/Model/Entity/User.php
+++ cms/src/Model/Entity/User.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace App\Model\Entity;
 
-use Authentication\PasswordHasher\DefaultPasswordHasher; // Add this line
+use Authentication\PasswordHasher\DefaultPasswordHasher; 
 use Cake\ORM\Entity;
 
 /**
@@ -12,8 +12,8 @@ use Cake\ORM\Entity;
  * @property int $id
  * @property string $email
  * @property string $password
- * @property \Cake\I18n\FrozenTime|null $created
- * @property \Cake\I18n\FrozenTime|null $modified
+ * @property \Cake\I18n\DateTime|null $created
+ * @property \Cake\I18n\DateTime|null $modified
  *
  * @property \App\Model\Entity\Article[] $articles
  */
@@ -28,7 +28,7 @@ class User extends Entity
      *
      * @var array<string, bool>
      */
-    protected $_accessible = [
+    protected array $_accessible = [
         'email' => true,
         'password' => true,
         'created' => true,
@@ -39,15 +39,19 @@ class User extends Entity
     /**
      * Fields that are excluded from JSON versions of the entity.
      *
-     * @var array<string>
+     * @var list<string>
      */
-    protected $_hidden = [
+    protected array $_hidden = [
         'password',
     ];
+
     protected function _setPassword(string $password) : ?string
     {
         if (strlen($password) > 0) {
             return (new DefaultPasswordHasher())->hash($password);
         }
+        return null;
     }
+
+
 }
